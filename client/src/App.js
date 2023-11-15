fb55a4dc1d1937153b98920c7bb573922e07170d
morningB <yjw001205@gmail.com>
2023-11-15 17:06:33 +0900

    main

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+
+import {Route, Routes} from "react-router-dom";
+import BoardList from "./routes/BoardList";
+import Home from "./routes/Home.js";
+import Acess from "./routes/Acess.js";
+import React from "react";
+
+function App() {
+ return (
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/board" element={<BoardList/>}/>
+      <Route path="/acess" element={<Acess/>}/>
+    </Routes>
+ );
+}
+
+export default App;
